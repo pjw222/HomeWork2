98b1e4b93b98f4acf49632875fe60dc66416ed6d
pjw222 <wpzktm@naver.com>
2023-09-13 15:43:07 +0900

    -230913 수업내용

--- src/c230912/Main.java
+++ src/c230912/Main.java
@@ -33,16 +33,68 @@ public class Main {
 		    - Structured Query Language
 		    변수 선언 int a;
 		  ## 명령어 
-		    - 구조 생성 create		     
+		    - 구조 생성 create	
+create table test(
+id number (10, 0),
+name varchar2(10),
+score number(10, 5),
+address varchar2(20)
+);
+
+	create table test2(
+	id number (10, 0) CONSTRAINT pk_id PRIMARY KEY,
+	name varchar2(10) NOT NULL,
+	score number(10, 5) DEFAULT 0,
+	address varchar2(20) 
+	);
+create table test3(
+id number (10, 0),
+name varchar2(10),
+score number(10, 5) DEFAULT 0,
+address varchar2(20), 
+create_at timestamp default to_date(sysdate, 'yyyy/mm/dd hh:mi:ss')
+);
+create table student(
+id number (10, 0) CONSTRAINT stpk_id PRIMARY KEY,
+name varchar2(200) DEFAULT '이름입력안함',
+idaddress varchar2(10) NOT NULL,
+passward varchar2(20) NOT NULL,
+age number (10, 0) DEFAULT 0,
+gitaddress varchar2(200) DEFAULT '깃주소입력안함'
+); 	     
+
+create table hobby(
+id number(10, 0)  CONSTRAINT hbpk_id PRIMARY KEY,
+name varchar2(100) NOT NULL,
+description varchar2(200) DEFAULT '내용',
+create_at timestamp default to_date(sysdate, 'yyyy/mm/dd hh:mi:ss'),
+averagetime number(20, 0),
+loacation varchar2(100)
+); 
+
+
+	- CONSTRAINT : 제약 조건
+
 		    - 구조 삭제 drop
 		    - 구조 수정(테이블) alter
 		    - 테이블 명 수정 rename
+
+
+		    
 		    
 //		  #### 데이터 조작어, DML   
 		    - 데이터 삽입 insert 
+		       - insert into 테이블명 (컬럼명, 컬럼명, ...) values (값, 값, ...);
 		    - 데이터 수정 update 
+		       - update 테이블명 set 컬렴영 = 값, 컬럼명 = 값, ... where 조건;
 		    - 데이터 삭제 delete 
+		      - delete from 테이블명 where 조건;
 		    - 데이터 선택 select 
+		      - select 컬렴명, 컬럼명, ... from 테이블명;
+		      - select * from user_tables;
+		      
+		       - select * from all_tables;  치면 지옥을 맛보게 됨
+		      
 		    
 		    - 권한 부여 grant
 		    - 권한 삭제 revoke
@@ -64,6 +116,19 @@ public class Main {
 		   #### 기타
 		      - Date => 날짜
 		      - Timestamp => 시간(기간)
+### Option
+    #### NOT NULL => (NULLABLE) (NULLABLE : 비워둘수있는가?)
+    #### DEFAULT : 기본값
+    #### UNIQUE  => 중복 체크
+      - NOT NULL이랑 같이 쓴다.
+    #### PRIMARY KEY : 기본키
+      - 테이블의 유일한 컬럼
+      - NOT NULL + UNIQUE 중복될수 없고 비워둘수 없다.
+      - 관계를 맺을 때 쓴다.
+      - id 
+    #### FOREIGN KEY : 외래키
+      - 관계를 맺을 때 쓴다.
+      - 관계에 대해서 이름정도로 생각하면 된다. ex) 선생님과 학생이 있을때 선생님이 학생을 가르쳐주는 관계라고 가정하면 이때 가르쳐주는게 왜래키 이름이다.		      
 
 테이블을 생성할거다 => create
 이름, 타입, 데이터의 길이를 정한다.
